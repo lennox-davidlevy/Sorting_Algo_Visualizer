439f14d3abe04024f54ca7676b51b2f41fe53632
David Levy <lennox.davidlevy@gmail.com>
2020-12-14 14:10:58 -0500

    create random array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Visualizer from './components/Visualizer';
 
 function App() {
   return (
     <div className="App">
     <h1>Sorting Visualizer</h1>
+    <Visualizer/>
     </div>
   );
 }
