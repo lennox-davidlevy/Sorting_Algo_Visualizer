d69c364d3d7b498acdd33455d2e9afd13912c837
David Levy <lennox.davidlevy@gmail.com>
2020-12-16 12:50:57 -0500

    animation first pass

--- src/algorithms.js
+++ src/algorithms.js
@@ -26,4 +26,50 @@ const merge = (left, right) => {
   return [...result, ...end];
 };
 
-export { mergeSort };
+const returnAnimationArray = (arr) => {
+  const result = [];
+  if (arr.length <= 1) return arr;
+  const tempArr = [...arr],
+    start = 0,
+    end = arr.length - 1;
+  // const start = 0
+  // const end = arr.length - 1;
+  mergeAnimationHelper(arr, start, end, tempArr, result);
+  return result;
+};
+
+const mergeAnimationHelper = (arr, start, end, tempArr, result) => {
+  if (start === end) return;
+  const mid = Math.floor((start + end) / 2);
+  mergeAnimationHelper(tempArr, start, mid, arr, result);
+  mergeAnimationHelper(tempArr, mid + 1, end, arr, result);
+  mergeAnimation(arr, start, mid, end, tempArr, result);
+};
+
+const mergeAnimation = (arr, start, mid, end, tempArr, result) => {
+  let i = start,
+    k = start,
+    j = mid + 1;
+  while (i <= mid && j <= end) {
+    result.push([i, j], [i, j]);
+    if (tempArr[i] <= tempArr[j]) {
+      result.push([k, tempArr[i]]);
+      arr[k++] = tempArr[i++];
+    } else {
+      result.push([k, tempArr[j]]);
+      arr[k++] = tempArr[j++];
+    }
+  }
+  while (i <= mid) {
+    result.push([i, i], [i, i]);
+    result.push([k, tempArr[i]]);
+    arr[k++] = tempArr[i++];
+  }
+  while (j <= end) {
+    result.push([j, j], [j, j]);
+    result.push([k, tempArr[j]]);
+    arr[k++] = tempArr[j++];
+  }
+};
+
+export { mergeSort, returnAnimationArray };
