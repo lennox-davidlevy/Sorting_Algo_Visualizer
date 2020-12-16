bf8d6de723c17ba653b09eba0291bdcf89e88b1d
David Levy <lennox.davidlevy@gmail.com>
2020-12-16 15:57:38 -0500

    added first pass bubblesort

--- src/components/algoAnimation.js
+++ src/components/algoAnimation.js
@@ -1,4 +1,4 @@
-import { returnAnimationArray } from '../algorithms';
+import { returnAnimationArray, bubbleSort } from '../algorithms';
 
 const animateMergeSortOnClick = (arr) => {
   const animations = returnAnimationArray(arr);
@@ -7,22 +7,41 @@ const animateMergeSortOnClick = (arr) => {
     const changeColor = i % 3 !== 2;
     if (changeColor) {
       const [firstBar, secondBar] = animations[i];
+
       const firstBarStyle = numBars[firstBar].style;
       const secondBarStyle = numBars[secondBar].style;
       const color = i % 3 === 0 ? 'red' : 'whitesmoke';
       setTimeout(() => {
         firstBarStyle.backgroundColor = color;
         secondBarStyle.backgroundColor = color;
-      }, i * 10);
+      }, i * 30);
     } else {
       setTimeout(() => {
         let [firstBar, newHeight] = animations[i];
         const firstBarStyle = numBars[firstBar].style;
         newHeight = (newHeight / 1000) * 100;
         firstBarStyle.height = `${newHeight}%`;
-      }, i * 10);
+      }, i * 30);
     }
   }
 };
 
-export { animateMergeSortOnClick };
+const animateBubbleSortOnClick = (arr) => {
+  const animations = bubbleSort(arr);
+  console.log(animations);
+  for (let i = 0; i < animations.length; i++) {
+    const numBars = document.getElementsByClassName('bar-number');
+    const [firstBar, secondBar, change] = animations[i];
+    const firstBarStyle = numBars[firstBar].style;
+    const secondBarStyle = numBars[secondBar].style;
+    if (change) {
+      setTimeout(() => {
+        const temp = firstBarStyle.height;
+        firstBarStyle.height = secondBarStyle.height;
+        secondBarStyle.height = temp;
+      }, i * 30);
+    }
+  }
+};
+
+export { animateMergeSortOnClick, animateBubbleSortOnClick };
